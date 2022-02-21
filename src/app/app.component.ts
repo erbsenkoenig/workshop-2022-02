440767e6e14b64b862e2a71d16d190a0d6cd40f2
Stephanie Katterwe <stephanie.katterwe@dpdhl.com>
2022-02-21 14:44:26 +0100

    first component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,38 @@
 import { Component, OnInit } from '@angular/core';
+import { Flight } from './types';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'flight-app',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent  {
+export class AppComponent {
   title = 'Hello World!';
+  from: string;
+  to: string;
+
+  flights: Flight[] = [];
+  selectedFlight: Flight | null = null;
+
+  constructor(private httpClient: HttpClient) {}
+
+  search() {
+    const url = 'http://www.angular.at/api/flight';
+
+    const params = {
+      from: this.from,
+      to: this.to,
+    };
+
+    const headers = { Accept: 'application/json' };
+
+    this.httpClient.get<Flight[]>(url, { params, headers }).subscribe((result: Flight[]) => {
+      this.flights = result;
+    });
+  }
+
+  select(flight: Flight) {
+    this.selectedFlight = flight;
+  }
 }
