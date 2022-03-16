cb6d277fdaae4deea7e7f232c2f0613ee1107198
Stephanie Katterwe <stephanie.katterwe@dpdhl.com>
2022-03-16 16:41:38 +0100

    add basic http test

--- src/app/flight-booking/flight-search/flight.service.spec.ts
+++ src/app/flight-booking/flight-search/flight.service.spec.ts
@@ -1,16 +1,45 @@
-import { TestBed } from '@angular/core/testing';
+import { fakeAsync, TestBed } from '@angular/core/testing';
 
 import { FlightService } from './flight.service';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
+import { BASE_URL, BASE_URL_WITH_NEW_SCOPING } from './constants';
 
 describe('FlightService', () => {
   let service: FlightService;
+  let testingController: HttpTestingController;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+      providers: [
+        {
+          provide: BASE_URL,
+          useValue: 'BASE_URL',
+        },
+        {
+          provide: BASE_URL_WITH_NEW_SCOPING,
+          useValue: 'SCOPING_BASE_URI',
+        },
+      ],
+    });
     service = TestBed.inject(FlightService);
+    testingController = TestBed.inject(HttpTestingController);
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
   });
+
+  it('should search flights and return results', fakeAsync(() => {
+    let result;
+
+    service.searchFlights('FROM', 'TO').subscribe((r: any) => {
+      result = r;
+    });
+
+    const request = testingController.expectOne('BASE_URL/flight?from=FROM&to=TO');
+    request.flush('FLIGHTS');
+
+    expect(result).toEqual('FLIGHTS');
+  }));
 });
